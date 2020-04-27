41023a82ffd0ac7d3e0fca11ac300581fd72f291
zivojin_milutinovic1998 <halkatran@gmail.com>
2020-04-27 23:55:35 +0200

    inicijalizovan babel i webpack

---
+++ Zivojin_Milutinovic_RWA1/webpack.config.js
@@ -0,0 +1,32 @@
+const path=require('path');
+
+module.exports=
+{
+  mode:'development',
+entry:'./src/index.js',
+devtool:'inline-source-map',
+output:{
+path:path.resolve(__dirname,'dist'),
+filename:'bundle.js',
+
+},
+module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+devServer:{
+    contentBase:'./' 
+}
+
+
+};
